fe232fa73fadcc610807275a8d9a35c943604311
Andrew Vasilchenko <cazonove@gmail.com>
2023-12-23 18:34:57 +0300

    added plugin choices.js and customiztion choices.js with bootstrap 5

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,37 @@
 // // Import vendor jQuery plugin example
 import bootstrap from 'bootstrap'
+import Choices from 'choices.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	function allowNumbersOnly() {
+		const formControlNumbers = document.querySelectorAll('.form-control-numbers');
+		
+		if (formControlNumbers.length > 0) {
+			formControlNumbers.forEach(function(input) {
+				input.addEventListener('input', function(event) {
+					const value = event.target.value;
+					const numbersOnly = value.replace(/\D/g, '');
+					event.target.value = numbersOnly;
+				});
+			});
+		}
+	}
 	
+	allowNumbersOnly();
+
+	const jsFormSelects = document.querySelectorAll('.js-form-select');
+
+	const selectConfig = {
+		placeholder: true,
+		searchEnabled: false,
+		shouldSort: false,
+		itemSelectText: '',
+	}
+
+	jsFormSelects.forEach((select) => {
+		const choices = new Choices(select, selectConfig)
+	})
+
 
 })
