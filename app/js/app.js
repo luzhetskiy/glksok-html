2722ba1027d26b49ebf40cbc3850bf4fde21f291
Andrew Vasilchenko <cazonove@gmail.com>
2023-12-26 03:30:43 +0300

    fix and added page signin.html

--- app/js/app.js
+++ app/js/app.js
@@ -5,51 +5,51 @@ import Choices from 'choices.js'
 document.addEventListener('DOMContentLoaded', () => {
 
 	function handleFormControlReset() {
-		const formControlResets = document.querySelectorAll('.form-control-reset');
-	
+		const formControlResets = document.querySelectorAll('.form-control-reset')
+
 		if (formControlResets.length > 0) {
-			formControlResets.forEach(function(formControlReset) {
-				const input = formControlReset.querySelector('input');
-				const btnReset = formControlReset.querySelector('.btn-reset');
-		
-				input.addEventListener('input', function() {
+			formControlResets.forEach(function (formControlReset) {
+				const input = formControlReset.querySelector('input')
+				const btnReset = formControlReset.querySelector('.btn-reset')
+
+				input.addEventListener('input', function () {
 					if (input.value.trim() !== '') {
-						formControlReset.classList.add('is-show');
+						formControlReset.classList.add('is-show')
 					} else {
-						formControlReset.classList.remove('is-show');
+						formControlReset.classList.remove('is-show')
 					}
 				});
-		
-				btnReset.addEventListener('click', function() {
-					input.value = '';
-					formControlReset.classList.remove('is-show');
+
+				btnReset.addEventListener('click', function () {
+					input.value = ''
+					formControlReset.classList.remove('is-show')
 				});
 			});
 		}
 	}
-	
+
 	function allowNumbersOnly() {
 		const formControlNumbers = document.querySelectorAll('.form-control-numbers');
-	
+
 		if (formControlNumbers.length > 0) {
-			formControlNumbers.forEach(function(input) {
-				input.addEventListener('input', function(event) {
-					const value = event.target.value;
-					const numbersOnly = value.replace(/\D/g, '');
-					event.target.value = numbersOnly;
-		
-					const formControlReset = input.closest('.form-control-reset');
+			formControlNumbers.forEach(function (input) {
+				input.addEventListener('input', function (event) {
+					const value = event.target.value
+					const numbersOnly = value.replace(/\D/g, '')
+					event.target.value = numbersOnly
+
+					const formControlReset = input.closest('.form-control-reset')
 					if (formControlReset) {
-						const resetInput = formControlReset.querySelector('input');
+						const resetInput = formControlReset.querySelector('input')
 						if (resetInput.value.trim() === '') {
-							formControlReset.classList.remove('is-show');
+							formControlReset.classList.remove('is-show')
 						}
 					}
-				});
-			});
+				})
+			})
 		}
 	}
-	
+
 	handleFormControlReset();
 	allowNumbersOnly();
 
@@ -65,6 +65,37 @@ document.addEventListener('DOMContentLoaded', () => {
 	jsFormSelects.forEach((select) => {
 		const choices = new Choices(select, selectConfig)
 	})
+
+	const tabOverflow = document.querySelector('#tabOverflow')
+	const signinTab = document.querySelector('#signin-tab')
+	const tabPaneCard = document.querySelector('#signin-tab-card')
+	const hiddenClass = 'd-none'
+
+	if (tabOverflow && signinTab && tabPaneCard) {
+		tabOverflow.addEventListener('show.bs.tab', handleTabOverflow)
+		signinTab.addEventListener('shown.bs.tab', handleSigninTab)
+		handleSigninTab()
+	}
+
+	function handleTabOverflow(event) {
+		if (event.target.id === 'signin-tab' && signinTab.classList.contains('active')) {
+			// tabPaneCard.classList.remove(showClass)
+			tabPaneCard.classList.add(hiddenClass)
+		} else {
+			tabPaneCard.classList.remove(hiddenClass)
+			// tabPaneCard.classList.add(showClass)
+		}
+	}
+
+	function handleSigninTab() {
+		if (signinTab.classList.contains('active')) {
+			// tabPaneCard.classList.remove(showClass)
+			tabPaneCard.classList.add(hiddenClass)
+		} else {
+			tabPaneCard.classList.remove(hiddenClass)
+			// tabPaneCard.classList.add(showClass)
+		}
+	}
 
 
 })
