631007eb23bd2aaa68c19d6c7bb7fa47b06bc679
Andrew Vasilchenko <cazonove@gmail.com>
2023-12-27 01:17:39 +0300

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -3,26 +3,86 @@ import Choices from 'choices.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	function restrictInputToHex() {
+		const formControlID = document.querySelectorAll('.form-control-id');
+
+		if (formControlID.length > 0) {
+			formControlID.forEach(function (input) {
+				input.addEventListener('input', function () {
+					const value = this.value.replace(/[^0-9A-Fa-f]/g, '');
+					this.value = value;
+
+					if (!value) {
+						this.classList.add('is-invalid');
+						this.parentElement.classList.add('is-invalid');
+					} else {
+						this.classList.remove('is-invalid');
+						this.parentElement.classList.remove('is-invalid');
+					}
+				});
+			});
+		}
+	}
+
 	function handleFormControlReset() {
-		const formControlResets = document.querySelectorAll('.form-control-reset')
+		const formControlResets = document.querySelectorAll('.form-control-reset');
 
 		if (formControlResets.length > 0) {
 			formControlResets.forEach(function (formControlReset) {
-				const input = formControlReset.querySelector('input')
-				const btnReset = formControlReset.querySelector('.btn-reset')
+				const input = formControlReset.querySelector('input');
+				const btnReset = formControlReset.querySelector('.btn-reset');
+				const form = formControlReset.closest('.needs-validation');
+				const btnSubmit = form.querySelector('.btn-disabled[type="submit"]');
+				const inputs = form.querySelectorAll('input:required');
+				const selects = form.querySelectorAll('select:required');
 
 				input.addEventListener('input', function () {
 					if (input.value.trim() !== '') {
-						formControlReset.classList.add('is-show')
+						formControlReset.classList.add('is-show');
 					} else {
-						formControlReset.classList.remove('is-show')
+						formControlReset.classList.remove('is-show');
+					}
+
+					if (checkAllInputsFilled()) {
+						if (btnSubmit) {
+							btnSubmit.removeAttribute('disabled');
+						}
+					} else {
+						if (btnSubmit) {
+							btnSubmit.setAttribute('disabled', 'disabled');
+						}
 					}
 				});
 
 				btnReset.addEventListener('click', function () {
-					input.value = ''
-					formControlReset.classList.remove('is-show')
+					input.value = '';
+					formControlReset.classList.remove('is-show');
+
+					if (checkAllInputsFilled()) {
+						if (btnSubmit) {
+							btnSubmit.removeAttribute('disabled');
+						}
+					} else {
+						if (btnSubmit) {
+							btnSubmit.setAttribute('disabled', 'disabled');
+						}
+					}
 				});
+
+				function checkAllInputsFilled() {
+					let allFieldsFilled = true;
+					inputs.forEach(input => {
+						if (input.value.trim() === '') {
+							allFieldsFilled = false;
+						}
+					});
+					selects.forEach(select => {
+						if (select.value.trim() === '') {
+							allFieldsFilled = false;
+						}
+					});
+					return allFieldsFilled;
+				}
 			});
 		}
 	}
@@ -33,29 +93,128 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (formControlNumbers.length > 0) {
 			formControlNumbers.forEach(function (input) {
 				input.addEventListener('input', function (event) {
-					const value = event.target.value
-					const numbersOnly = value.replace(/\D/g, '')
-					event.target.value = numbersOnly
+					const value = event.target.value;
+					const numbersOnly = value.replace(/\D/g, '');
+					event.target.value = numbersOnly;
 
-					const formControlReset = input.closest('.form-control-reset')
+					const formControlReset = input.closest('.form-control-reset');
 					if (formControlReset) {
-						const resetInput = formControlReset.querySelector('input')
+						const resetInput = formControlReset.querySelector('input');
 						if (resetInput.value.trim() === '') {
-							formControlReset.classList.remove('is-show')
+							formControlReset.classList.remove('is-show');
 						}
 					}
-				})
-			})
+
+					if (!/^\d{1,}$/.test(numbersOnly)) {
+						this.classList.add('is-invalid');
+						this.parentElement.classList.add('is-invalid');
+					} else {
+						this.classList.remove('is-invalid');
+						this.parentElement.classList.remove('is-invalid');
+					}
+				});
+			});
 		}
 	}
 
+	restrictInputToHex();
 	handleFormControlReset();
 	allowNumbersOnly();
 
+	const formsValidation = document.querySelectorAll('.needs-validation');
+
+	if (formsValidation.length > 0) {
+		Array.from(formsValidation).forEach(form => {
+			const btnSubmit = form.querySelector('.btn-disabled[type="submit"]');
+			const inputs = form.querySelectorAll('input:required');
+			const selects = form.querySelectorAll('select:required');
+
+			function checkAllInputsFilled() {
+				let allFieldsFilled = true;
+				inputs.forEach(input => {
+					if (input.value.trim() === '') {
+						allFieldsFilled = false;
+					}
+				});
+				selects.forEach(select => {
+					if (select.value.trim() === '') {
+						allFieldsFilled = false;
+					}
+				});
+				return allFieldsFilled;
+			}
+
+			form.addEventListener('submit', event => {
+				inputs.forEach(input => {
+					if (input.value.trim() === '') {
+						input.classList.add('is-invalid');
+						input.parentElement.classList.add('is-invalid');
+					}
+				});
+
+				selects.forEach(select => {
+					if (select.value.trim() === '') {
+						select.parentElement.parentElement.classList.add('is-invalid');
+						select.parentElement.parentElement.parentElement.classList.add('is-invalid');
+					}
+				});
+
+				if (!form.checkValidity() || !checkAllInputsFilled()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+			});
+
+			inputs.forEach(input => {
+				input.addEventListener('input', function () {
+					if (this.value.trim() === '') {
+						this.classList.add('is-invalid');
+						this.parentElement.classList.add('is-invalid');
+					} else {
+						this.classList.remove('is-invalid');
+						this.parentElement.classList.remove('is-invalid');
+					}
+
+					if (checkAllInputsFilled()) {
+						if (btnSubmit) {
+							btnSubmit.removeAttribute('disabled');
+						}
+					} else {
+						if (btnSubmit) {
+							btnSubmit.setAttribute('disabled', 'disabled');
+						}
+					}
+				});
+			});
+
+			selects.forEach(select => {
+				select.addEventListener('change', function () {
+					if (this.value.trim() === '') {
+						this.parentElement.parentElement.classList.add('is-invalid');
+						this.parentElement.parentElement.parentElement.classList.add('is-invalid');
+					} else {
+						this.parentElement.parentElement.classList.remove('is-invalid');
+						this.parentElement.parentElement.parentElement.classList.remove('is-invalid');
+					}
+
+					if (checkAllInputsFilled()) {
+						if (btnSubmit) {
+							btnSubmit.removeAttribute('disabled');
+						}
+					} else {
+						if (btnSubmit) {
+							btnSubmit.setAttribute('disabled', 'disabled');
+						}
+					}
+				});
+			});
+		});
+	}
 
 
-	const filtersInput = document.querySelector('.filters');
-	const tableFilters = document.querySelectorAll('[data-filter]');
+
+	const filtersInput = document.querySelector('.filters')
+	const tableFilters = document.querySelectorAll('[data-filter]')
 
 	if (filtersInput && tableFilters.length > 0) {
 		function applyFilters() {
